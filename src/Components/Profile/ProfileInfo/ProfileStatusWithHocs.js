a99e124229fd86c7b74f16b950d29cce86c4579e
Alexandr <alexandrpo271@gmail.com>
2021-08-29 18:01:45 +0300

    refactor ProfileStatus component with hoc UseEffect

--- src/Components/Profile/ProfileInfo/ProfileStatusWithHocs.js
+++ src/Components/Profile/ProfileInfo/ProfileStatusWithHocs.js
@@ -1,9 +1,12 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export const ProfileStatusWithHoc = (props) => {
 
     const [editMode, setEditMode] =  useState(false);
     const [status, setStatus] =  useState(props.status);
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
     const activateMode = () => {setEditMode(true);}
     const deactivateMode = () => {
         setEditMode(false);
